9cc5cffc2c69c52423e4b9578628e8169aad43b4
Claire Parker <claire.parker87@gmail.com>
2016-11-04 23:31:24 +0000

    :movie_camera: Minify scss with Gulp and use in site

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    rename = require('gulp-rename'),
     cssnano = require('gulp-cssnano'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     cache = require('gulp-cache');
     
 gulp.task('styles', function() {
-  return cssnano()
-    .pipe(gulp.dest('assets/css'));
+    gulp.src('css/pixyll.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('css/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('css/'));
 });
 
 gulp.task('images', function() {
   return gulp.src('assets/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe(gulp.dest('assets/'));
+});
+
+gulp.task('default', [], function() {
+    gulp.start('styles', 'images');
 });
