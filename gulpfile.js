edc79e687e0cde3d931f6f195824465b1851d5ec
Claire Parker <claire.parker87@gmail.com>
2016-11-15 21:39:49 +0000

    :rage4: Add sourcemaps to minified CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     cache = require('gulp-cache'),
-    uglifycss = require('gulp-uglifycss');
-    
+    sourcemaps = require('gulp-sourcemaps')
+    cssnano = require('gulp-cssnano');
+
 gulp.task('css', function() {
     gulp.src('scss/pixyll.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('css/'))
         .pipe(rename({suffix: '.min'}))
-        .pipe(uglifycss())
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('css/'));
 });
 
